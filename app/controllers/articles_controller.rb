1e9f48008f9d0c949fca8be8c3b95326fed6d30e
teknofage <teknofage@gmail.com>
2020-10-01 12:23:13 -0700

    can show individual articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,9 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+    def show
+        @article = Article.find(params[:id])
+    end
     private
         def article_params
         params.require(:article).permit(:title, :description)
