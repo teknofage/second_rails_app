cef4125ff5638409ae5f90fa13f589afca93ee79
teknofage <teknofage@gmail.com>
2020-10-06 02:58:57 -0700

    fixed @obj typo in _errors.html

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20201006083603) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+    create_table "articles", force: :cascade do |t|
+        t.string "title"
+        t.text "description" 
+        t.datetime "created_at"
+        t.datetime "updated_at"
+        t.integer "user_id"
+    end
+    
+    create_table "users", force: :cascade do |t|
+        t.string "username"
+        t.string "email"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.string "password_digest"
+    end
+    
 end
