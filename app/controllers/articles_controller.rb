c1edcfefc3f7a097d3fa4281d7843990cb2b9ace
teknofage <teknofage@gmail.com>
2020-10-06 03:17:56 -0700

    added user authentication in cli

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,11 +44,11 @@ class ArticlesController < ApplicationController
     end
 
     private
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
 
-    def set_article
-        @article = Article.find(params[:id])
-    end
+        def set_article
+            @article = Article.find(params[:id])
+        end
 end
