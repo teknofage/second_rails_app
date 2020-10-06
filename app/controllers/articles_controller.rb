b3691bd7f50e8dca6393671c3672fa1fa24e9e4d
teknofage <teknofage@gmail.com>
2020-10-06 00:17:06 -0700

    styled index and show pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -24,17 +24,17 @@ class ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
-            flash[:notice] = "Article was updated"
+            flash[:success] = "Article was updated"
             redirect_to article_path(@article)
         else
-            flash[:notice] = "Article was not updated"
+            flash[:success] = "Article was not updated"
             render 'edit'
         end
     end
 
     def destroy
         @article.destroy
-        flash[:notice] = "Article was deleted"
+        flash[:success] = "Article was deleted"
         redirect_to articles_path
     end
 
