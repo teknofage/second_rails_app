ccf79f313b3a384905480155a35be1244f450e88
teknofage <teknofage@gmail.com>
2020-10-06 03:57:52 -0700

    added gravatar and user-article functionality, but users page not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pages#index'
-get 'about', to: 'pages#about'
-resources :articles
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'pages#index'
+    get 'about', to: 'pages#about'
+    get 'signup', to: 'users#new'
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
+    resources :users, except: [:new]
+
+    resources :articles
 end
